a7a285bb39d2284a90596b99abb3f278d1cba62d
abhi6403 <163630847+abhi6403@users.noreply.github.com>
2025-02-11 13:22:07 +0530

    Bullet Explosion

---
+++ Assets/Scripts/Shell/ShellController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellController : MonoBehaviour
+{
+    public ParticleSystem explosion;
+    private void OnCollisionEnter(Collision collision)
+    {
+        SpawnDamageParticles();
+        Explode();
+    }
+
+    private void Explode()
+    {
+        Destroy(gameObject);
+    }
+
+    private void SpawnDamageParticles()
+    {
+        Instantiate(explosion, transform.position, Quaternion.identity);
+    }
+    
+}
